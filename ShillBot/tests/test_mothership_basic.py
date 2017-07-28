f3e67c60dc5a07e9c33108724274c846047f4af2
SethTolkamp <stolkamp@uvic.ca>
2017-07-28 12:54:16 -0700

    new build

--- ShillBot/tests/test_mothership_basic.py
+++ ShillBot/tests/test_mothership_basic.py
@@ -1,8 +1,13 @@
-
 import unittest
+import codecs
+import os
 
 from mothership.base import MothershipServer
 
 
 class TestMothershipBasic(unittest.TestCase):
+
+	def test_basic_mothership_connection(self):
+		mother = MothershipServer()
+
     pass
